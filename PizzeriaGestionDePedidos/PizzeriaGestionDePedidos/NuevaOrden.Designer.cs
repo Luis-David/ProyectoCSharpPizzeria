92f83c1db992846eaa5d4b1aede0fc1b258ccb99
damia <damia@DESKTOP-8LDQBS9>
2020-02-14 17:14:29 -0600

    Nueva Orde

---
+++ PizzeriaGestionDePedidos/PizzeriaGestionDePedidos/NuevaOrden.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PizzeriaGestionDePedidos
+{
+    partial class NuevaOrden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.pizzasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refrescosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grandeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medianaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cocaColaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pepsiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 48);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pizzasToolStripMenuItem,
+            this.refrescosToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip3.TabIndex = 3;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // pizzasToolStripMenuItem
+            // 
+            this.pizzasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grandeToolStripMenuItem,
+            this.medianaToolStripMenuItem,
+            this.chicaToolStripMenuItem});
+            this.pizzasToolStripMenuItem.Name = "pizzasToolStripMenuItem";
+            this.pizzasToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.pizzasToolStripMenuItem.Text = "Pizzas";
+            // 
+            // refrescosToolStripMenuItem
+            // 
+            this.refrescosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cocaColaToolStripMenuItem,
+            this.spriteToolStripMenuItem,
+            this.pepsiToolStripMenuItem});
+            this.refrescosToolStripMenuItem.Name = "refrescosToolStripMenuItem";
+            this.refrescosToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.refrescosToolStripMenuItem.Text = "Refrescos";
+            // 
+            // grandeToolStripMenuItem
+            // 
+            this.grandeToolStripMenuItem.Name = "grandeToolStripMenuItem";
+            this.grandeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grandeToolStripMenuItem.Text = "Grande";
+            // 
+            // medianaToolStripMenuItem
+            // 
+            this.medianaToolStripMenuItem.Name = "medianaToolStripMenuItem";
+            this.medianaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.medianaToolStripMenuItem.Text = "Mediana";
+            // 
+            // chicaToolStripMenuItem
+            // 
+            this.chicaToolStripMenuItem.Name = "chicaToolStripMenuItem";
+            this.chicaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chicaToolStripMenuItem.Text = "Chica";
+            // 
+            // cocaColaToolStripMenuItem
+            // 
+            this.cocaColaToolStripMenuItem.Name = "cocaColaToolStripMenuItem";
+            this.cocaColaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cocaColaToolStripMenuItem.Text = "Coca cola";
+            // 
+            // spriteToolStripMenuItem
+            // 
+            this.spriteToolStripMenuItem.Name = "spriteToolStripMenuItem";
+            this.spriteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.spriteToolStripMenuItem.Text = "Sprite";
+            // 
+            // pepsiToolStripMenuItem
+            // 
+            this.pepsiToolStripMenuItem.Name = "pepsiToolStripMenuItem";
+            this.pepsiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pepsiToolStripMenuItem.Text = "Pepsi";
+            // 
+            // NuevaOrden
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip3);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "NuevaOrden";
+            this.Text = "NuevaOrden";
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem pizzasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grandeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medianaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chicaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refrescosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cocaColaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spriteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pepsiToolStripMenuItem;
+    }
+}
