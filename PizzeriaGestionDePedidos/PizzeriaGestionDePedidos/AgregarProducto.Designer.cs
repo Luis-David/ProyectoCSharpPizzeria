b29e1a6720c017093fcfda56452b621fe066a227
Luis David <luisdaviduacm@gmail.com>
2020-02-14 19:00:34 -0600

    Agregar producto

---
+++ PizzeriaGestionDePedidos/PizzeriaGestionDePedidos/AgregarProducto.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace PizzeriaGestionDePedidos
+{
+    partial class AgregarProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbPrecio = new System.Windows.Forms.TextBox();
+            this.tbSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbSize = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.rbPizza = new System.Windows.Forms.RadioButton();
+            this.rbRefresco = new System.Windows.Forms.RadioButton();
+            this.rbOtro = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbIngredientes = new System.Windows.Forms.TextBox();
+            this.lbIngredientes = new System.Windows.Forms.Label();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(149, 38);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 1;
+            // 
+            // tbPrecio
+            // 
+            this.tbPrecio.Location = new System.Drawing.Point(149, 67);
+            this.tbPrecio.Name = "tbPrecio";
+            this.tbPrecio.Size = new System.Drawing.Size(100, 20);
+            this.tbPrecio.TabIndex = 2;
+            // 
+            // tbSize
+            // 
+            this.tbSize.Location = new System.Drawing.Point(149, 136);
+            this.tbSize.Name = "tbSize";
+            this.tbSize.Size = new System.Drawing.Size(100, 20);
+            this.tbSize.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Precio:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbSize
+            // 
+            this.lbSize.AutoSize = true;
+            this.lbSize.Location = new System.Drawing.Point(94, 139);
+            this.lbSize.Name = "lbSize";
+            this.lbSize.Size = new System.Drawing.Size(49, 13);
+            this.lbSize.TabIndex = 6;
+            this.lbSize.Text = "Tamaño:";
+            this.lbSize.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // rbPizza
+            // 
+            this.rbPizza.AutoSize = true;
+            this.rbPizza.Location = new System.Drawing.Point(94, 129);
+            this.rbPizza.Name = "rbPizza";
+            this.rbPizza.Size = new System.Drawing.Size(50, 17);
+            this.rbPizza.TabIndex = 7;
+            this.rbPizza.TabStop = true;
+            this.rbPizza.Text = "Pizza";
+            this.rbPizza.UseVisualStyleBackColor = true;
+            this.rbPizza.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbRefresco
+            // 
+            this.rbRefresco.AutoSize = true;
+            this.rbRefresco.Location = new System.Drawing.Point(94, 161);
+            this.rbRefresco.Name = "rbRefresco";
+            this.rbRefresco.Size = new System.Drawing.Size(68, 17);
+            this.rbRefresco.TabIndex = 8;
+            this.rbRefresco.TabStop = true;
+            this.rbRefresco.Text = "Refresco";
+            this.rbRefresco.UseVisualStyleBackColor = true;
+            // 
+            // rbOtro
+            // 
+            this.rbOtro.AutoSize = true;
+            this.rbOtro.Location = new System.Drawing.Point(94, 195);
+            this.rbOtro.Name = "rbOtro";
+            this.rbOtro.Size = new System.Drawing.Size(45, 17);
+            this.rbOtro.TabIndex = 9;
+            this.rbOtro.TabStop = true;
+            this.rbOtro.Text = "Otro";
+            this.rbOtro.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbDescripcion);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbIngredientes);
+            this.panel1.Controls.Add(this.tbIngredientes);
+            this.panel1.Controls.Add(this.tbNombre);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbPrecio);
+            this.panel1.Controls.Add(this.tbSize);
+            this.panel1.Controls.Add(this.lbSize);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(217, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 280);
+            this.panel1.TabIndex = 10;
+            // 
+            // tbIngredientes
+            // 
+            this.tbIngredientes.Location = new System.Drawing.Point(149, 174);
+            this.tbIngredientes.Name = "tbIngredientes";
+            this.tbIngredientes.Size = new System.Drawing.Size(100, 20);
+            this.tbIngredientes.TabIndex = 7;
+            // 
+            // lbIngredientes
+            // 
+            this.lbIngredientes.AutoSize = true;
+            this.lbIngredientes.Location = new System.Drawing.Point(75, 174);
+            this.lbIngredientes.Name = "lbIngredientes";
+            this.lbIngredientes.Size = new System.Drawing.Size(68, 13);
+            this.lbIngredientes.TabIndex = 8;
+            this.lbIngredientes.Text = "Ingredientes:";
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(584, 365);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btAgregar.TabIndex = 11;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Descripción:";
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(149, 104);
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.tbDescripcion.TabIndex = 10;
+            // 
+            // AgregarProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rbOtro);
+            this.Controls.Add(this.rbRefresco);
+            this.Controls.Add(this.rbPizza);
+            this.Name = "AgregarProducto";
+            this.Text = "AgregarProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbPrecio;
+        private System.Windows.Forms.TextBox tbSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbSize;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.RadioButton rbRefresco;
+        private System.Windows.Forms.RadioButton rbPizza;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbIngredientes;
+        private System.Windows.Forms.TextBox tbIngredientes;
+        private System.Windows.Forms.RadioButton rbOtro;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.Label label3;
+    }
+}
